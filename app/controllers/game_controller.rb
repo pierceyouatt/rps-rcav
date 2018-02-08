864605d413cdb0536fde2cd56f7cd2cc45bec01d
pierceyouatt <pierce.youatt@chicagobooth.edu>
2018-02-08 13:17:37 -0500

    Complete RPS

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,10 +3,13 @@ class GameController < ApplicationController
     @computer_move = ["rock","paper","scissors"].sample
     if @computer_move == "rock"
       @result = "tied"
+      @boxcolor = "warning"
     elsif @computer_move == "paper"
       @result = "lost"
+      @boxcolor = "danger"
     else
       @result = "won"
+      @boxcolor = "success"
     end
     render("/calc_templates/play_rock_view.html.erb")
   end
@@ -14,10 +17,13 @@ class GameController < ApplicationController
     @computer_move = ["rock","paper","scissors"].sample
     if @computer_move == "rock"
       @result = "won"
+      @boxcolor = "success"
     elsif @computer_move == "paper"
       @result = "tied"
+      @boxcolor = "warning"
     else
       @result = "lost"
+      @boxcolor = "danger"
     end
     render("/calc_templates/play_paper_view.html.erb")
   end
@@ -25,11 +31,17 @@ class GameController < ApplicationController
     @computer_move = ["rock","paper","scissors"].sample
     if @computer_move == "rock"
       @result = "lost"
+      @boxcolor = "danger"
     elsif @computer_move == "paper"
       @result = "won"
+      @boxcolor = "success"
     else
       @result = "tied"
+      @boxcolor = "warning"
     end
     render("/calc_templates/play_scissors_view.html.erb")
+  end
+  def rules
+    render("/calc_templates/rules_view.html.erb")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get("/rock",{:controller => "game", :action => "play_rock"})
   get("/paper",{:controller => "game", :action => "play_paper"})
   get("/scissors",{:controller => "game", :action => "play_scissors"})
-  
+  get("/",{:controller => "game", :action => "rules"})
 end
